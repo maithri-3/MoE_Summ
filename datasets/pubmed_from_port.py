eb9bf097444b19144c4348fb1db7a73a3c54b809
Maithri Muralikrishna <m.muralikrishna.cse21@itbhu.ac.in>
2026-01-27 12:12:25 +0530

    Create pubmed_from_port.py

---
+++ datasets/pubmed_from_port.py
@@ -0,0 +1,32 @@
+import pdb
+import random
+from datasets import load_dataset
+# from datasets import load_metric
+import json
+
+dataset = load_dataset(
+    "qiaojin/PubMedQA",
+    "3.0.0",
+    revision="main",
+    download_mode="reuse_cache_if_exists"
+)
+
+
+train_dataset = dataset['train']
+fw=open('pubmed_train.json','w')
+for case in train_dataset:
+    ARTICLE = case['article']
+    highlights = case['highlights']
+    content={}
+
+    content['src']=ARTICLE
+    src_length=len(ARTICLE.split())
+    content['tgt']=highlights
+    tgt_length=len(highlights.split())
+    if tgt_length!=0:
+        ratio=int(src_length/tgt_length)
+        if ratio==0 or ratio==1:
+            continue
+        content['idx']='cnndm'
+        json.dump(content,fw)
+        fw.write('\n')
